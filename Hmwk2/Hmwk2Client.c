02b868005879f9bb562494c663ea0f63b44a27be
Scott_Mueller <sjmule@comcast.net>
2015-02-19 11:27:02 -0600

    Client_SegFault_during_Ping_Read

--- Hmwk2/Hmwk2Client.c
+++ Hmwk2/Hmwk2Client.c
@@ -6,21 +6,36 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define DEBUG 1
+
 struct sockaddr_in ServAddr, ClientAddr;
 
 int main(int args, char** argv)
 {
 	int fd;
 	int i = 0;
-	char packet[1024];
-	char message[] = "Hello World";
+	char packet[1472];
 	
 	//Ping the server to determine round trip time
 	char *pinging = "ping -w 2 ";
-	char str[80];
+	char *pingingEnd = " |tail -1| awk '{print $4}' | cut -d '/' -f 2 > tmp.txt";
+	char str[256];
 	strcpy(str, pinging);
 	strcat(str, argv[1]);
+	strcat(str, pingingEnd);
 	system(str);
+	
+	printf("I pinged\n");
+	
+	//Get the results of the ping
+	char avgS[7];
+	double avg = 0;
+	int t = NULL;
+	FILE* tmp = fopen("tmp.txt", "r");
+	getline(&avgS, &t, tmp);
+	printf("%s\n", avgS);
+	avg = atof(avgS);
+	printf("%d\n", avg);
 	
 	//Construct the server address
 	ServAddr.sin_family = AF_INET;
@@ -34,11 +49,18 @@ int main(int args, char** argv)
 	
 	fd = socket(AF_INET, SOCK_DGRAM, 0);
 	
+	char junk[1469];
+	int j = 0;
+	for(j; j<1469; j++)
+	{
+		junk[j] = 'a';
+	}
+	
 	for(;;)
 	{
 		sprintf(packet, "%d ", i);
-		strcat(packet, message);
-		sendto(fd, packet, 1024, 0, &ServAddr, sizeof(ServAddr));
+		strcat(packet, junk);
+		sendto(fd, packet, 1472, 0, &ServAddr, sizeof(ServAddr));
 		i++;
 	}
 	
