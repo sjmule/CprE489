e5937bebc9b088517bc91e8af103a9740cf02e4b
Scott_Mueller <sjmule@comcast.net>
2015-02-20 14:43:15 -0600

    :poop:

--- lab3.c
+++ lab3.c
@@ -7,15 +7,15 @@
 #include <netinet/in.h>
 
 //generate random number, if < packet loss, don't send packet
-//source port 54809 destination port 54810
+//source port 44444 destination port 33333
 struct sockaddr_in SourceAddr, DestinationAddr;
 
 int main(int args, char** argv)
 {
 	char buff[2048]; //Buffer to hold the packets in
 	int n, fdS, fdD;
-	int DestAddrLen; //The destination address length
-	int SourceAddrLen; //The source address length
+	socklen_t DestAddrLen; //The destination address length
+	socklen_t SourceAddrLen; //The source address length
 	int lossRate = atoi(argv[5]);  //Determine the loss rate
 	int random;
 
@@ -34,31 +34,29 @@ int main(int args, char** argv)
 	DestinationAddr.sin_addr.s_addr = inet_addr(argv[3]);
 
 	//Get the size of both the addresses
-	DestAddrLen = sizeof(DestAddrLen);
+	DestAddrLen = sizeof(DestinationAddr);
 	SourceAddrLen = sizeof(SourceAddr);
 	
 	//Open the sockets for the source and destination, bind the source address
 	fdS = socket(AF_INET, SOCK_DGRAM, 0);
 	fdD = socket(AF_INET, SOCK_DGRAM, 0);
-	bind(fdS, &SourceAddr, SourceAddrLen);
+	bind(fdS, (struct sockaddr*)&SourceAddr, SourceAddrLen);
 
 	for(;;)
 	{
 		//receive and store a packet from the source
-		n = recvfrom(fdS, buff, 2048, 0, &SourceAddr, &SourceAddrLen);
+		n = recvfrom(fdS, buff, sizeof(buff), 0, (struct sockaddr*) &SourceAddr, &SourceAddrLen);
 		
-		printf("%d ", n);
 		//generate a random value 0-100
 		srand(time(NULL));
 		random = rand() % 101;
-		//printf("%d ", random);
 		//check if the random value is less than the loss rate, if it is, don't send the packet, if it is not, send the packet
-		if(random <= lossRate);
+		if(random <= lossRate)
 			continue;
 		else
 		{
 			//send the packet to the destination
-			sendto(fdD, buff, n, 0, &DestinationAddr, DestAddrLen);
+			sendto(fdD, buff, n, 0, (struct sockaddr*)&DestinationAddr, DestAddrLen);
 		}
 	}
 	return 0;
