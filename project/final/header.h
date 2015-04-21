545b991777440af6430308eeb55a505625fe1e44
Scott_Mueller <sjmule@comcast.net>
2015-04-20 20:17:23 -0500

    1429579042

--- project/final/header.h
+++ project/final/header.h
@@ -18,6 +18,7 @@
 #include <string.h>
 #include <pthread.h>
 #include <semaphore.h>
+#include <argp.h>
 
 #define ReportError(msg)       {perror(msg); exit(-1);}
 #define DEBUG 1   //used for debugging, set to 1 to print debugging information
@@ -27,7 +28,7 @@ void srandom(unsigned);
 //void IntroduceError(char*, double);
 //char *GenerateData(int);
 int Server(int), Client(int);
-int select(int);
+//int select(int);
 double floor(double);
 char linebuffer[100];
 
--- project/final/main.c
+++ project/final/main.c
@@ -1,7 +1,81 @@
 #include "header.h"
 
-int server_fd, client_fd;
+// program version string
+const char *argp_program_version = "Token Ring Client 1.0";
+// program bug address string
+const char *argp_program_bug_address = "Scott Mueller <scotm1@iastate.edu>";
+// program documentation string
+static char doc[] = "Simple token ring workstation client";
 
+// list of options supported
+static struct argp_option options[] = 
+{
+	{"verbose", 'v', 0, 0, "Produce verbose output"},
+	{"quite", 'q', 0, 0, "Don't produce any output"},
+	{"silent", 's', 0, OPTION_ALIAS, 0},
+	{"node-id", 'n', "ID", 0, "Character to identify this workstation by: a, b, or c"},
+	{"server-port", 'h', "Port", 0, "The port number to host the server on"},
+	{"client-port", 'c', "Port", 0, "The port number to connect to a server on"},
+	{0}
+};
+
+//arugment structure to store the results of command line parsing
+struct arguments
+{
+	// are we in verbose mode?
+	int verbose_mode;
+	// the node id
+	char node_id;
+	// the server port address
+	int server_port;
+	// the client port address
+	int client_port;
+};
+
+/*
+ * @brief     Callback to parse a command line argument
+ * @param     key
+ *                 The short code key of this argument
+ * @param     arg
+ *                 The argument following the code
+ * @param     state
+ *                 The state of the arg parser state machine
+ * @return    0 if succesfully handeled the key, ARGP_ERR_UNKONWN if the key was uknown
+ */
+error_t parse_opt(int key, char *arg, struct argp_state *state)
+{
+	struct arguments *arguments = state->input;
+	switch(key)
+	{
+		case 'v':
+			arguments->verbose_mode = 2;
+			break;
+		case 'q':
+		case 's':
+			arguments->verbose_mode = 0;
+			break;
+		case 'n':
+			arguments->node_id = arg[0];
+			break;
+		case 'h':
+			arguments->server_port = atoi(arg);
+			break;
+		case 'c':
+			arguments->client_port = atoi(arg);
+			break;
+		default:
+			return ARGP_ERR_UNKNOWN;
+	}
+	return 0;
+}
+
+// The arg parser object
+static struct argp argp = {&options, parse_opt, 0, doc};
+
+// The server socket needs to be global so the thread can listen on it
+int server_fd;
+
+// The thread for listening on our socket
 void listener(void)
 {
 
@@ -9,7 +83,14 @@ void listener(void)
 
 int main(int argc, char** argv)
 {
-	int server_port, client_port, n, er;
+	struct arguments arguments;
+	arguments.verbose_mode = 1;
+	arguments.server_port = 0;
+	arguments.client_port = 0;
+	arguments.node_id = '0';
+	argp_parse(&argp, argc, argv, 0, 0, &arguments);
+
+	int client_fd, n, er;
 	int err = 0;
 	char DLE = 16;
     char SYN = 22;
@@ -19,34 +100,42 @@ int main(int argc, char** argv)
 	char *buffer = malloc(80 * sizeof(char));
 	size_t *t = 0;
 	size_t size = 0;
-	node_id = argv[1][0];
 
-	//printf("Please enter node id:\n");
-	//scanf("%c", node_id);
-	if(DEBUG) printf("Node id: %c\n", node_id);
-	
-	printf("Please enter the port to run the server on:\n");
-	while((n = getline(&buffer, &t, stdin)) < 0);
-	server_port = atoi(buffer);
-	if(DEBUG) printf("Server port: %d\n", server_port);
-
-	printf("Please enter the port to connect as a client on:\n");
-	while((n = getline(&buffer, &t, stdin)) < 0);
-	client_port = atoi(buffer);
-	if(DEBUG) printf("Client port: %d\n", client_port);
-
-	if(node_id == 'c')
+	if(arguments.server_port == 0)
 	{
-		client_fd = Client(client_port);
-		server_fd = Server(server_port);
+		printf("Please enter a port to run the server on using \"-h #####\"\n");
+		exit(-1);
+	}
+	if(arguments.client_port == 0)
+	{
+		printf("Please enter a port to run the client on using \"-c #####\"\n");
+		exit(-1);
+	}
+	if(arguments.node_id == '0')
+	{
+		printf("Please specify a node id using \"-n \%c\" where \%c is either: a, b, or c\n");
+		exit(-1);
+	}
+
+	if(arguments.verbose_mode > 0)
+	{
+		printf("Node id: %c\n", arguments.node_id);
+		printf("Running server on port: %d\n", arguments.server_port);
+		printf("Connecting to client on port: %d\n", arguments.client_port);
+	}
+
+	if(arguments.node_id == 'c')
+	{
+		client_fd = Client(arguments.client_port);
+		server_fd = Server(arguments.server_port);
 	}
 	else
 	{
-		server_fd = Server(server_port);
-		client_fd = Client(client_port);
+		server_fd = Server(arguments.server_port);
+		client_fd = Client(arguments.client_port);
 	}
 
-	if(DEBUG) printf("Attempting to create socket listener thread\n");
+	if(arguments.verbose_mode == 2) printf("Attempting to create socket listener thread\n");
 	pthread_t socket_listener;
 	err = pthread_create(&socket_listener, NULL, (void*)listener, NULL);
 	if(err != 0)
@@ -54,6 +143,7 @@ int main(int argc, char** argv)
 		perror("pthread_create encountered an error");
 		exit(-1);
 	}
+	if(arguments.verbose_mode == 2) printf("Attempting to join socket listener thread\n");
 	err = pthread_join(socket_listener, NULL);
 	if(err != 0)
 	{
@@ -63,14 +153,14 @@ int main(int argc, char** argv)
 
 	for(;;)
 	{
-		if(node_id == 'a')
+		if(arguments.node_id == 'a')
 		{
 			printf("When ready to enter a message please type in the destination node address (a,b,or c)\n");
 
         	i = fgetc(stdin);
         	fgetc(stdin);
 
-        	printf("When ready enter message of length 72 characters to send to node %c:",i);
+        	printf("When ready enter message of length 72 characters to send to node %c:\n",i);
 
         	er = getline(&buffer, &size, stdin);
         	printf("er=%d\n",er);
@@ -82,7 +172,7 @@ int main(int argc, char** argv)
 				break;
 			}
 		}
-		else if(node_id == 'b')
+		else if(arguments.node_id == 'b')
 		{
 			n = read(server_fd, buffer, 80);
 			printf("%s\n", buffer);
