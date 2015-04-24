9dc6ed28b1dcd77527fc4c104cc653960ebbfa41
Scott Mueller <sjmule@comcast.net>
2015-04-24 14:48:03 -0500

    fixing

--- project/final/Client.c
+++ project/final/Client.c
@@ -1,13 +1,4 @@
-/*****************************************************************************/
-/*                                                                           */
-/* This routine establishes an active open connection.  That is, it creates  */
-/* a socket, and connects using it to a remote machine. The routine returns  */
-/* a file descriptor to be used to communicate with the remote machine.      */
-/* Make sure that you change the machine name from "vulcan" to that of   	 */
-/* the remote machine.  Also, change the port number to a suitable port      */
-/* number as indicated in the project writeup.                               */
-/*                                                                           */
-/*****************************************************************************/
+#include "header.h"
 
 int Client(int port)
 {
@@ -34,7 +25,10 @@ int Client(int port)
 	otherhost = gethostbyname(otherhostname);
 	bcopy(otherhost->h_addr_list[0], &otheraddr.sin_addr, otherhost->h_length);
 
-	n = connect(s, &otheraddr, sizeof(otheraddr));
+	if(DEBUG) printf("Attempting to connect to client on port: %d\n", port);
+	while((n = connect(s, &otheraddr, sizeof(otheraddr))) != 0);
+
+	printf("Connected\n");
 
 	if ( n < 0)
 		return(n);
--- project/final/Server.c
+++ project/final/Server.c
@@ -1,14 +1,4 @@
-/*****************************************************************************/
-/*                                                                           */
-/* This routine establishes a passive open connection.  That is, it creates  */
-/* a socket, and passively wait for a connection.  Once a connection request */
-/* has been received, it echoes "connected" on the screen, and return        */
-/* a file descriptor to be used to communicate with the remote machine.      */
-/* Make sure that you change the machine name from "vulcan" to that you    	 */
-/* will be running your process on. Also, change the port number to          */
-/* a suitable port number as indicated in the project writeup.               */
-/*                                                                           */
-/*****************************************************************************/
+#include "header.h"
 
 int Server(int port)
 {
@@ -22,21 +12,24 @@ int Server(int port)
 
 	pid_t pid;
 
-	if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) ReportError ("socket");
+	if ((s = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) printf("No go server\n");
 	myname = gethostbyname(hostname);
 
 	bzero(&myaddr, sizeof(myaddr));
 	myaddr.sin_family  = AF_INET;
-	myaddr.sin_port = htons(port);
+	myaddr.sin_port = htons(port); //54841
 	bcopy(myname->h_addr_list[0], &myaddr.sin_addr, myname->h_length);
 	bind(s, &myaddr, sizeof(myaddr));
 
 	listen(s, 1);
+	if(DEBUG) printf("Server listening on port: %d\n", port);
 
 	otherlength = sizeof(otheraddr);
 	fd = accept(s, &otheraddr, &otherlength);
 
-	fprintf(stdout, "Connected");
+	printf("Connected\n");
+
+	close(s);
 
 	return(fd);
 }
